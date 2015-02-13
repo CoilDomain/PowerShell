6bb245e0e1fd12fa9f36e23e02bcf4785e1896be
unknown <jasonruiz@DC-JasonR.dtcorp.com>
2015-02-13 09:51:05 -0500

    no message

--- Snippets/DynamicParams.ps1
+++ Snippets/DynamicParams.ps1
@@ -4,15 +4,11 @@
     DynamicParam {
         $TemplatesAttributeCollection = new-object -Type System.Collections.ObjectModel.Collection[System.Attribute]
 
-        # [parameter(mandatory=...,
-        #     ...
-        # )]
         $TemplatesParameterAttribute = new-object System.Management.Automation.ParameterAttribute
         $TemplatesParameterAttribute.Mandatory = $true
         $TemplatesParameterAttribute.HelpMessage = "Enter A Template name"
         $TemplatesAttributeCollection.Add($TemplatesParameterAttribute)    
 
-        # [ValidateSet[(...)]
         $TemplatesNames = @()
         foreach($TemplatesInfo in Get-SCVMTemplate) {
             $TemplatesNames += $TemplatesInfo.Name
@@ -20,7 +16,6 @@
         $TemplatesValidateSetAttribute = New-Object -type System.Management.Automation.ValidateSetAttribute($TemplatesNames)
         $TemplatesAttributeCollection.Add($TemplatesValidateSetAttribute)
 
-        # Remaining boilerplate
         $TemplatesRuntimeDefinedParam = new-object -Type System.Management.Automation.RuntimeDefinedParameter("Template", [String[]], $TemplatesAttributeCollection)
 
         $paramDictionary = new-object -Type System.Management.Automation.RuntimeDefinedParameterDictionary
