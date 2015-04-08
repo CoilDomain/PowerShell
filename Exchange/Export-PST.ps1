719f0c893cc73f772ca6de7f5772609d51990741
unknown <jasonruiz@DC-JasonR.dtcorp.com>
2015-04-08 10:19:26 -0400

    no message

--- Exchange/Export-PST.ps1
+++ Exchange/Export-PST.ps1
@@ -1,7 +1,7 @@
-$Users=Get-Mailbox | Where-Object {$_.DistinguishedName -like "*OUNAME*"}
+$Users=Get-Mailbox | Where-Object {($_.DistinguishedName -like "*Disabled Users*") -and ($_.DistinguishedName -notlike "*unofficial*")}
 
 Function New-PSTExportJob ($UserName){ 
-$FullPath="\\FileServer\MailExport\"+$UserName+".pst"
+$FullPath="\\MailServer\MailExport\"+$UserName+".pst"
 New-MailboxExportRequest -Mailbox $UserName -FilePath $FullPath -Suspend
 }
 
@@ -10,7 +10,7 @@ New-PSTExportJob -Username $User.samaccountName
 }
 
 Function Start-ExportJobs ($Rate) {
-Get-MailboxExportRequest
+Write-Output (Get-MailboxExportRequest -Status completed | measure).count
 If (!(Get-MailboxExportRequest -Status InProgress)) { 
 Get-MailboxExportRequest | Where-Object {$_.Status -match "Suspended"} | Select -First $Rate | Resume-MailboxExportRequest
 Sleep 30
@@ -20,6 +20,7 @@ ElseIf ((Get-MailboxExportRequest -Status InProgress)) {
 Sleep 30
 Start-ExportJobs -Rate $Rate
 }
+ElseIf (!(Get-MailboxExportRequest -Status InProgress,Queued,Suspended)) {}
 }
 
 Start-ExportJobs -Rate 2
