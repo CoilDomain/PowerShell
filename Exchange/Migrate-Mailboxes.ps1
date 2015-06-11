62bcdd5d0fab695ee20957539ddae9e9e659bd54
unknown <jasonruiz@DC-JASONR.dtcorp.com>
2015-06-11 15:32:18 -0400

    no message

--- Exchange/Migrate-Mailboxes.ps1
+++ Exchange/Migrate-Mailboxes.ps1
@@ -1,12 +1,14 @@
-Get-MoveRequest | Where-Object {$_.Status -match "Completed"} | Remove-MoveRequest
-$Users=Get-Mailbox | Where-Object {$_.OrganizationalUnit -notlike "*Disabled*"} | Where-Object {$_.ServerName -notlike "ahcex2010db*"} | Where-Object {$_.Database -notlike "SSD Mailbox Database*"} | Where-Object {$_.MailboxMoveTargetMDB -notmatch "SSD Mailbox Database*"} | Where-Object {$_.Name -notmatch "ArchiveMgr_Journal"} | Select -first 20
+Get-MoveRequest | Where-Object {$_.Status -match "Completed"} | Remove-MoveRequest -Confirm:$false
+$Users=Get-Mailbox | Where-Object {$_.OrganizationalUnit -notlike "*Disabled*"} | Where-Object {$_.ServerName -notlike "ahcex2010db*"} | Where-Object {$_.Database -notlike "SSD Mailbox Database*"} | Where-Object {$_.MailboxMoveTargetMDB -notmatch "SSD Mailbox Database*"} | Where-Object {$_.Name -notmatch "ArchiveMgr_Journal"} | Select -first 8
 Foreach ($User in $Users) {
-New-MoveRequest -Identity $User.SamAccountName -TargetDatabase "SSD Mailbox Database 2"
+$Database=(Get-MailboxDatabase -Status | Where-Object {$_.Name -like "*SSD*"} | Sort-Object -Property Databasesize)[0].name
+New-MoveRequest -Identity $User.SamAccountName -TargetDatabase $Database
 }
 Function Check-Status {
 If ((Get-MoveRequest | Where-Object {$_.Status -match "InProgress"})) {
 Get-MoveRequest | Where-Object {$_.Status -match "InProgress"} | Get-MoveRequestStatistics | Sort-Object -Property PercentComplete -Descending
 sleep 60
+Clear
 Check-Status
 }
 Else{}
