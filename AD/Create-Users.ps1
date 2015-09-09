f9cf117e9163020fbaebd9a4de08e445a0e46920
unknown <jasonruiz@DC-JASONR.dtcorp.com>
2015-09-09 08:53:06 -0400

    OU Placement and Description

--- AD/Create-Users.ps1
+++ AD/Create-Users.ps1
@@ -2,4 +2,7 @@ $accounts=import-csv .\users.csv
 $accounts | foreach {
 $Username=$_.SamAccountName+"@"+(get-ADDomain).dnsroot
 $DisplayName=$_.GivenName+" "+$_.Surname
-New-ADuser -UserPrincipalName $username -Name $DisplayName -DisplayName $DisplayName -GivenName $_.GivenName -Surname $_.Surname -PasswordNeverExpires $true -AccountPassword  ("Password123" | ConvertTo-SecureString -AsPlainText -Force) -EmailAddress $_.emailaddress -Enabled $True}
+$Company="Company"
+$OU=Get-ADOrganizationalUnit -Filter * | Where-Object {$_.DistinguishedName -like "*$Company*"}
+$Description=$Description=$Company+" User"
+New-ADuser -Description $Description -UserPrincipalName $username -Name $DisplayName -DisplayName $DisplayName -GivenName $_.GivenName -Surname $_.Surname -PasswordNeverExpires $true -AccountPassword  ($_.Password | ConvertTo-SecureString -AsPlainText -Force) -Enabled $True -Path $OU.DistinguishedName}
