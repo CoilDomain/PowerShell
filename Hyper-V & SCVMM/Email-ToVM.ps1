7ff4cafa41b6a0fa736ffefcd43b327e57973de9
unknown <jasonruiz@DC-JASONR.dtcorp.com>
2015-10-20 17:14:21 -0400

    Don't need an extra line in the body now for formatting.

--- Hyper-V & SCVMM/Email-ToVM.ps1
+++ Hyper-V & SCVMM/Email-ToVM.ps1
@@ -57,10 +57,10 @@ Function Get-OutlookInBox
 }
 $Email=(Get-OutlookInBox | where-object {$_.subject -eq "VM Creation"} | select -first 1 -Property body | fl *)
 $String=$Email | Out-String
-$SubString=$String.Substring(20) | Out-String
-$VMName=($SubString.Substring(10,16)).Trim()
-$VMCPU=($SubString.Substring(30,30)).Trim()
-$VMRAM=($SubString.Substring(44,44)).Trim()
+$SubString=$String.Replace('Body : ',"`n")
+$VMName=($SubString.Substring(1,16)).Trim()
+$VMCPU=($SubString.Substring(30,25)).Trim()
+$VMRAM=($SubString.Substring(44,39)).Trim()
 $CDRIVE=($SubString.Substring(70,40)).Trim()
 $DDRIVE=($SubString.Substring(110,44)).Trim()
 If ($VMName -like "Name*" -and $VMCPU -like "CPU*" -and $VMRAM -like "RAM*" -and $CDRIVE -like "CDrive*" -and $DDRIVE -like "DDrive*"){
